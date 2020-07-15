617a823569eebb92711c5bc2038ab55cc631b6e9
angsikiong <50168702+angsikiong@users.noreply.github.com>
2020-07-15 17:02:14 +0800

    Initial Commit

---
+++ Wubai and China Blue's album/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wubai and China Blue's album
+//
+//  Created by tantsunsin on 2020/7/15.
+//  Copyright © 2020 tantsunsin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
